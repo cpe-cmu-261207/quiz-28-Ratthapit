8473ae8c5e8258e82a1de4694ee561d60b7bc4ea
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2022-08-04 14:10:53 +0700

    incomplete

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//const Ran = document.getElementById("btn-random");
+
+//Ran.onclick = () => {
+callApi();
+//};
+
+const { default: axios } = require("axios");
+
 async function callApi() {
-  const resp = await axios.get("https://randomuser.me/api/");
+  try {
+    const resp = await axios.get("https://randomuser.me/api/");
+    console.log(resp.data);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
